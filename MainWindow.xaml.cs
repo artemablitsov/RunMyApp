e4b625dc37ca38da8a2b74153277b3536e049956
Artem Ablitsov <artem.ablitsov@onestepbeyond.ru>
2020-02-01 21:41:21 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RunMyApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.DataContext = new MainWindowModel(true);
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 1 && e.ChangedButton == MouseButton.Left && Keyboard.Modifiers != ModifierKeys.Control)
+            {
+                var path = ((MainWindowModel)this.DataContext).AppToRun;
+                if (!string.IsNullOrEmpty(path))
+                {
+                    Process.Start(path);
+                }
+            } else if (e.ClickCount == 1 && e.ChangedButton == MouseButton.Left && Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                DragMove();
+            } else if (e.ClickCount == 2 && e.ChangedButton == MouseButton.Right)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
---
+++ MainWindowModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace RunMyApp
+{
+    public class MainWindowModel : INotifyPropertyChanged
+    {
+        public MainWindowModel(bool isFirstRun = false)
+        {
+            if (isFirstRun)
+            {
+                string baseDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                if (!File.Exists(Path.Combine(baseDir, "settings.json")))
+                {
+                    File.WriteAllText(Path.Combine(baseDir, "settings.json"), JsonConvert.SerializeObject(this, Formatting.Indented));
+                }
+                var settings = JsonConvert.DeserializeObject<MainWindowModel>(File.ReadAllText(Path.Combine(baseDir, "settings.json")));
+                Image = settings.Image;
+                AppToRun = settings.AppToRun;
+                if (!File.Exists(Image))
+                {
+                    throw new Exception("No image file found at: " + Image);
+                } else if (!Image.Contains("\\") && !Image.Contains("/"))
+                {
+                    Image = Path.Combine(baseDir, Image);
+                }
+                Bitmap img = new Bitmap(Image);
+                Width = img.Width;
+                Height = img.Height;
+            }
+        }
+
+        private int m_Width { get; set; }
+        public int Width
+        {
+            get => m_Width;
+            set
+            {
+                m_Width = value;
+                OnProperyChanged("Width");
+            }
+        }
+        private int m_Height { get; set; }
+        public int Height
+        {
+            get => m_Height;
+            set
+            {
+                m_Height = value;
+                OnProperyChanged("Height");
+            }
+        }
+
+        private string m_Image { get; set; }
+        public string Image {
+            get => m_Image; 
+            set
+            {
+                m_Image = value;
+                OnProperyChanged("Image");
+            } 
+        }
+        private string m_AppToRun { get; set; }
+        public string AppToRun {
+            get => m_AppToRun;
+            set
+            {
+                m_AppToRun = value;
+                OnProperyChanged("AppToRun");
+            }
+        }
+
+
+        #region Implementation of INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnProperyChanged(string _name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(_name));
+            }
+        }
+        #endregion
+    }
+}
